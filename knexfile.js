57ba95141390d8d5a520bb6279eee588afdaee6f
troikas <dimiaggel@gmail.com>
2021-03-24 21:32:51 +0200

    heroku pg

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,9 @@ module.exports = {
     production: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        },
         pool: {
             min: 2,
             max: 10,
