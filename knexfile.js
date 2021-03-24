984aca5b585c9b205f5d82c168d9f9e9cf7daf1e
troikas <dimiaggel@gmail.com>
2021-03-24 22:15:56 +0200

    Change knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,9 +16,6 @@ module.exports = {
     production: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        },
         pool: {
             min: 2,
             max: 10,
